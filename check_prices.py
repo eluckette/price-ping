745522359da24c53fee330aba982fe2ea3d9aa23
eluckette <emily.luckette@gmail.com>
2015-05-27 13:06:12 -0700

    added helper function setText for showing search results

--- check_prices.py
+++ check_prices.py
@@ -3,7 +3,7 @@ import amazonproduct
 from model import User, Product, Alert, PriceReport, connect_to_db, db
 from datetime import datetime
 import threading 
-
+from twilio.rest import TwilioRestClient 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -26,26 +26,43 @@ amazon_api_config = {
 
 api = amazonproduct.API(cfg=amazon_api_config)
 
+
+ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID'] 
+AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+ 
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN) 
+
 def check_prices():
 
 	date_now = datetime.utcnow()
 	active_alerts = session.query(Alert).filter(date_now < Alert.expiration_date).all()
 
+
 	for alert in active_alerts:
 		current_search = api.item_lookup(alert.product.asin, MerchantId='Amazon', ResponseGroup='Offers, Images, ItemAttributes')
 
 		if alert.product.price > current_search.Items.Item.Offers.Offer.OfferListing.Price.Amount:
-			print 'Send notification for: ',alert.product.title
-			print 'Original price: ',alert.product.price
-			print 'New price: ', current_search.Items.Item.Offers.Offer.OfferListing.Price.Amount
+			title = alert.product.title
+			origninal_price = alert.product.price
+			new_price = current_search.Items.Item.Offers.Offer.OfferListing.Price.Amount
+			
+			send_text(title, origninal_price, new_price)
 		
-		else:
-			price_report = PriceReport(alert_id=alert.alert_id, asin=str(alert.product.asin),
-									   price=int(current_search.Items.Item.Offers.Offer.OfferListing.Price.Amount), 
-									   date_checked=datetime.utcnow())
+		price_report = PriceReport(alert_id=alert.alert_id, asin=str(alert.product.asin),
+								   price=int(current_search.Items.Item.Offers.Offer.OfferListing.Price.Amount), 
+								   date_checked=datetime.utcnow())
 
-			session.add(price_report)
-			session.commit()
+		session.add(price_report)
+		session.commit()
+
+def send_text(title, origninal_price, new_price):
+
+		client.messages.create(
+		to="3153825951", 
+		from_="+14159643618", 
+		body="Price Alert!", title, "has just lowered in price"  
+		)
+
 
 def do_every(interval, function, iterations):
 	if iterations != 0:
